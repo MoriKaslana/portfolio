ea96f76c091185ba4f9f6362647d8e8cb4a980ce
unknown <ishomnabil@gmail.com>
2025-07-16 12:55:39 +0700

    images fix

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -8,7 +8,7 @@ title: 'MHW Wiki',
       description: 'A mystical website thay help thy new player to acknowledge MHW.',
       tech: ['XHTML, CSS, JavaScript, Firebase'],
       status: 'Legendary',
-      image: 'src/components/assets/mhw.png',
+      image: 'public/assets/mhw.png',
       link: 'https://github.com/MoriKaslana/MHW-Wiki'
     },
     {
@@ -16,7 +16,7 @@ title: 'MHW Wiki',
       description: 'An otherworldly assists for those whom need IT and Medical Solutions.',
       tech: ['HTML, HTML5, CSS, JavaScript'],
       status: 'Epic',
-      image: 'src/components/assets/alzain.png',
+      image: 'public/assets/alzain.png',
       link: 'https://github.com/AlZain-IT/alzain'
     },
     {
@@ -24,7 +24,7 @@ title: 'MHW Wiki',
       description: 'An e-commerce platform for whom those in need of boost in game currency.',
       tech: ['HTML, CSS, JavaScript, Node.js'],
       status: 'Epic',
-      image: 'src/components/assets/velkstore.png',
+      image: 'public/assets/velkstore.png',
       link: 'https://github.com/MoriKaslana/VelkStore'
     }
   ];
